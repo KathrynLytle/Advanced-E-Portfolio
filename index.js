071c763bdb624604e389bd9d85f1477c8bc7f4d4
Kathryn <kathrynlytle@hotmail.com>
2023-02-15 14:19:33 -0600

    m

--- index.js
+++ index.js
@@ -1,39 +1,42 @@
-// template_tvenkgoo
+// template_tvenkgo
 // service_fvub3ni 
 // 5MuPbSN7wg6H0Zfmu
 
- //function contact(event) {
-    //event.preventDefault(); 
-   // const loading = document.querySelector('.modal__overlay--loading');
-    //const success = document.querySelector('.modal__overlay--success');
-   // loading.classList += " modal__overlay--visible";
-   // emailjs 
-      //  .sendForm(
-         //   'service_fvub3ni',
-         //   'template_tvenkgoo',
-         //   event.target, 
-          //  'user_5MuPbSN7wg6H0Zfmu'
-       // ).then (() => {
-         //   loading.classList.remove("modal__overlay--visible");
-         //   success.classList += " modal__overlay--visible";
-       // }).catch(() => {
-        //    loading.classList.remove("modal__overlay--visible");
-        //    alert(
-         //       "The email service is temporarily unavailable. Please contact me directly at kathrynlytle@hotmail.com"
-       //     );
-     //   })
-//}
-
-
-function contact(event) {
-    event.preventDefault();
-    emailjs
-    .sendForm(
-        'service_fvub3ni',
-        'template_tvenkgoo',
-        event.target,
-        'user_5MuPbSN7wg6H0Zfmu'
-    ).then(() => {
-        console.log('this worked')
-    })
+ function contact(event) {
+    event.preventDefault(); 
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList += " modal__overlay--visible";
+    emailjs 
+        .sendForm(
+            'service_fvub3ni',
+            'template_tvenkgo',
+            event.target, 
+            '5MuPbSN7wg6H0Zfmu'
+        ).then (() => {
+           loading.classList.remove("modal__overlay--visible");
+            success.classList += " modal__overlay--visible";
+        }).catch(() => {
+            loading.classList.remove("modal__overlay--visible");
+            alert(
+                "The email service is temporarily unavailable. Please contact me directly at kathrynlytle@hotmail.com"
+            );
+        })
 }
+
+
+// function contact(event) {
+//     event.preventDefault();
+//     emailjs
+//     .sendForm(
+//         'service_fvub3ni',
+//         'template_tvenkgo',
+//         event.target,
+//         '5MuPbSN7wg6H0Zfmu'
+//     ).then(() => {
+//         console.log('this worked')
+//     })
+//     setTimeout(() => {
+//         console.log('it worked 1')
+//     }, 500)
+// }
